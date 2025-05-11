0781fcbd4488a2cb2b49e8d662eab6fda5fd3e93
sandeep mehra <sandeepmehra2259@gmail.com>
2025-05-11 22:53:44 +0530

    add new files

--- Learning-DSA-Classes/src/Recursion/Arrays/FindArraySortedOrNot.java
+++ Learning-DSA-Classes/src/Recursion/Arrays/FindArraySortedOrNot.java
@@ -2,6 +2,13 @@ package Recursion.Arrays;
 
 public class FindArraySortedOrNot {
     public static void main(String[] args) {
-
+       int[] arr ={1,2,3,4,5,6,7,8,9};
+        System.out.println(result(arr,0));
+    }
+    static boolean result(int[] arr,int index){
+        if(index==arr.length-1){
+            return true;
+        }
+        return arr[index]<arr[index+1]&&result(arr,index+1);
     }
 }
