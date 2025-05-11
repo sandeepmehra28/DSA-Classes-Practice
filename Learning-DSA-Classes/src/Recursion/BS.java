42715c0b234af1f25e4c8ec4d81e39b6d5d13dc3
sandeep mehra <sandeepmehra2259@gmail.com>
2025-05-11 22:02:06 +0530

    add new files

--- Learning-DSA-Classes/src/Recursion/BS.java
+++ Learning-DSA-Classes/src/Recursion/BS.java
@@ -20,4 +20,18 @@ public class BS {
         }
         return search(arr, target, mid + 1, end);
     }
+
+    public static class Concept {
+        public static void main(String[] args) {
+         Con(5);
+        }
+        static void Con(int n){
+            if(n==0){
+                return ;
+            }
+            System.out.println(n);
+             Con(--n);//print n in decreasing order
+            //Con(n--);//postDecrease give a stackOverflow exception
+        }
+    }
 }
