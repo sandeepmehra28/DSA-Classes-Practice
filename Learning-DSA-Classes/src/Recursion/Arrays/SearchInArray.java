1e90575871b60600257dfe8e56cb2d53e626daab
sandeep mehra <sandeepmehra2259@gmail.com>
2025-05-12 19:51:35 +0530

    add new files

--- Learning-DSA-Classes/src/Recursion/Arrays/SearchInArray.java
+++ Learning-DSA-Classes/src/Recursion/Arrays/SearchInArray.java
@@ -1,10 +1,15 @@
 package Recursion.Arrays;
+
+import java.util.ArrayList;
+
 public class SearchInArray {
     public static void main(String[] args) {
-        int[] arr ={1,2,3,4,55,6,7,8,9,10};
-        int target =9;
+        int[] arr ={1,2,3,4,55,6,7,8,8,8,9,10};
+        int target =8;
         System.out.println(findAns(arr,target,0));
         System.out.println(findIndex(arr,target,0));
+        findIndexUsingArrayList(arr,target,0);
+        System.out.println(list);
     }
     static boolean findAns(int[] arr, int target, int index){
         if(index== arr.length){
@@ -21,5 +26,15 @@ public class SearchInArray {
         }else{
            return findIndex(arr,target,index+1);
         }
+    }
+    static ArrayList<Integer> list =new ArrayList<>();
+    static void findIndexUsingArrayList(int[] arr, int target, int index){
+        if(index==arr.length){
+            return;
+        }
+        if(arr[index]==target){
+            list.add(index);
+        }
+        findIndexUsingArrayList(arr,target,index+1);
     }
 }
