4dbd39804df19a1fa72fb91e21d702d1a45f8cf9
sandeep mehra <sandeepmehra2259@gmail.com>
2025-05-25 00:59:29 +0530

    add new files

---
+++ Learning-DSA-Classes/src/Recursion/Strings/permutation.java
@@ -0,0 +1,47 @@
+package Recursion.Strings;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class permutation {
+    public static void main(String[] args) {
+//        permu("","ABC");
+//        findPermutation("ABSG");
+        ArrayList<String> permutations = findPermutation("aa");
+        System.out.println(permutations);
+    }
+    static void permu(String p,String up){
+        if(up.length()==0){
+            System.out.println(p);
+            return;
+        }
+        for (int i = 0; i < up.length(); i++) {
+        char ch = up.charAt(i);
+        String left = up.substring(0,i);
+        String right = up.substring(i+1);
+        String finalAns =left+right;
+        permu(p+ch,finalAns);
+        }
+
+    }
+    public static ArrayList<String> findPermutation(String s) {
+        HashSet<String> uniquePermutations = helperPrem("", s);
+        return new ArrayList<>(uniquePermutations);
+    }
+    static  HashSet<String> helperPrem(String p, String up){
+        HashSet<String> result = new HashSet<>();
+        if(up.length()==0){
+            result.add(p);
+            return result;
+        }
+        for (int i = 0; i < up.length(); i++) {
+            char ch =up.charAt(i);
+            String left = up.substring(0,i);
+            String right =up.substring(i+1);
+            String finalAns =left+right;
+            result.addAll(helperPrem(ch+p,finalAns));
+        }
+
+        return result;
+    }
+}
---
+++ Learning-DSA-Classes/src/Recursion/Strings/prem2.java
@@ -0,0 +1,34 @@
+package Recursion.Strings;
+
+import java.util.HashSet;
+
+public class prem2 {
+    public static void main(String[] args) {
+      String s1 ="abc";
+      String s2 ="lecabee";
+        System.out.println(checkInclusion(s1,s2));
+    }
+    public static boolean checkInclusion(String s1, String s2) {
+        HashSet<String> uniquePrem = helper("",s2);
+        if(s1.equals(uniquePrem)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    static HashSet<String> helper(String p, String up){
+        HashSet<String> result = new HashSet<>();
+        if(up.length()==0){
+            result.add(p);
+            return result;
+        }
+        for (int i = 0; i < up.length(); i++) {
+            char ch = up.charAt(i);
+            String l =up.substring(0,i);
+            String r =up.substring(i+1);
+            String finalAns = l+r;
+            result.addAll(helper(p+ch,finalAns));
+        }
+        return result;
+    }
+}
