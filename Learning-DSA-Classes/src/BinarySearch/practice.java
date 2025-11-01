bcfe11b177509f62a939462ecced643d603c8c92
sandeep mehra <sandeepmehra2259@gmail.com>
2025-11-01 22:45:15 +0530

    update and add files.

--- Learning-DSA-Classes/src/BinarySearch/practice.java
+++ Learning-DSA-Classes/src/BinarySearch/practice.java
@@ -4,28 +4,50 @@ import java.util.ArrayList;
 import java.util.Collections;
 public class practice {
     public static void main(String[] args) {
-    ArrayList<Integer> a = new ArrayList<>();
-    a.add(0,1);
-        a.add(1,2);
-        a.add(2,3);
-        a.add(3,4);
-        a.add(4,5);
-        a.add(5,6);
-        int m = 3;
-        System.out.println(a);
-        reverseArray(a,m);
-        System.out.println(a);
+    int[] a = {1, 3, 5, 5, 5, 5, 67, 123, 125};
+        int m = 5;
+        System.out.println(find(a,m));
     }
-    public static void reverseArray(ArrayList<Integer> arr, int m)
+   static ArrayList<Integer> find(int[] arr, int m)
     {
-        int start = m+1;
-        int end = arr.size()-1;
-        while (start<=end){
-        Collections.swap(arr,start,end);
-        start++;
-        end--;
+        ArrayList<Integer> ans = new ArrayList<>();
+        ans.add(firstIndex(arr,m));
+        ans.add(lastIndex(arr,m));
+        return ans;
     }
-
+    static int firstIndex(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+        int ans = -1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(target==arr[mid]){
+                ans = mid;
+                end = mid-1;
+            }else if(target>arr[mid]){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+    static int lastIndex(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+        int ans = -1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(target==arr[mid]){
+                ans = mid;
+                start = mid+1;
+            }else if(target>arr[mid]){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return ans;
     }
 
 }
